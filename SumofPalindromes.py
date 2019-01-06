c945ac2c12115798e89fac5145abf038eb5e2c22
mudasirdar <46322380+mudasirdar@users.noreply.github.com>
2019-01-07 01:23:54 +0530

    Update SumofPalindromes.py
    Changes done in palindrome sum

--- SumofPalindromes.py
+++ SumofPalindromes.py
@@ -1,53 +1,20 @@
-# converts number to base  
-# k by changing it into string. 
-def integer_to_string(n, base): 
-  
-    str = ""; 
-    while (n > 0): 
-        digit = n % base; 
-        n = int(n / base); 
-        str = chr(digit + ord('0')) + str; 
-    return str; 
-  
-# function to check for palindrome 
-def isPalindrome(i, k): 
-    temp = i; 
-      
-    # m stores reverse of a number 
-    m = 0; 
-    while (temp > 0): 
-        m = (temp % 10) + (m * 10); 
-        temp = int(temp / 10); 
-      
-    # if reverse is equal to number 
-    if (m == i): 
-      
-        # converting to base k 
-        str = integer_to_string(m, k); 
-        str1 = str; 
-      
-        # reversing number in base k 
-        # str=str[::-1]; 
-      
-        # checking palindrome 
-        # in base k 
-        if (str[::-1] == str1): 
-            return i; 
-    return 0; 
-  
-# function to find sum of palindromes 
-def sumPalindrome(n, k): 
-      
-    sum = 0; 
-    for i in range(n):  
-        sum += isPalindrome(i, k); 
-    print("Total sum is", sum); 
-  
-# Driver code 
-n = 1000000; 
-k = 2; 
-  
-sumPalindrome(n, k); 
-  
-# This code is contributed  
-# by mits  
+#check if Base 10 is palindrome
+def palindrome_decimal(num):
+    a = str(num) 
+    return a == a[::-1]
+
+
+#check if Base 2 is palindrome
+def palindrome_binary(num):
+    a = str(bin(num))[2:]
+    return a == a[::-1]
+
+# Returns true if both bases are plaindrome
+def check_palindrome(num):
+    return palindrome_decimal(num) and palindrome_binary(num)
+
+#calculates the sum of palindrome
+def sum_of_palindrome(lower_limit, upper_limit):
+    return sum(x for x in range(lower_limit, upper_limit+1) if check_palindrome(x))
+    
+print('sum of numbers less than 1 million that are palindrome in both bases is:\n',sum_of_palindrome(1,1000000))
